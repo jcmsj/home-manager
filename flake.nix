0fc962b84c1f97354432251f592d5f5610330b42
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2024-03-21 07:07:29 +0800

    Feat: setup home-manager

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Home Manager configuration of jcsan";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # add ags
+    ags.url = "github:Aylur/ags";
+  };
+
+  outputs = { nixpkgs, home-manager, ags, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+
+      homeConfigurations."jcsan" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # Specify your home configuration modules here, for example,
+
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+          ags.homeManagerModules.default  # another could be ags
+         ];
+
+      };
+    };
+}
